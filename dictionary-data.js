84b14d6e933f050cc5f5f3024cb04edd49a74490
RaiCCya <126196159+RaiCCya@users.noreply.github.com>
2025-06-24 08:25:58 -0600

    Test. (Overhaul)
    Complete overhaul; automatic dictionary.
    
    Planned features:
    - Dark Mode
    - Study Tab
    - About Tab
    
    coming soon...

---
+++ dictionary-data.js
@@ -0,0 +1,26 @@
+const dictionaryData = [
+  {
+    jp: "日本語",
+    kana: "にほんご",
+    fil: "Wikang Hapon",
+    type: "Noun",
+    meaning: "Japanese language",
+    exampleJP: "日本語を勉強しています。",
+    exampleFIL: "Nag-aaral ako ng wikang Hapon."
+  },
+  {
+    jp: "食べる",
+    kana: "たべる",
+    fil: "Kumain",
+    type: "Verb",
+    meaning: "to eat",
+    exampleJP: "ご飯を食べます。",
+    exampleFIL: "Kumakain ng kanin."
+  }]
+    jp: "水",
+    kana; "みず"
+    fil: "tubig"
+    type: "Noun"
+    meaning: "Water"
+    exampleJP:水をください
+    exampleFIL: "tubig po."
